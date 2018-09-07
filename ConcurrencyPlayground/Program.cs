a90c8a38917c0b8a1d94413cd42fcddb697676e3
Paul Freedman <paul.freedman19@googlemail.com>
2018-09-07 22:11:37 +0100

    Add project files.

---
+++ ConcurrencyPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConcurrencyPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
